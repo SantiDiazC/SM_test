fc6d6194acdc9eddeef92f2ce1ad5df32a60cf7f
SantiDiazC <dsdiazc@unal.edu.co>
2023-06-07 16:55:41 +0900

    modify ch1->2

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import cv2
 
 ch0 = nidaqmx.Task()
 ch0.ai_channels.add_ai_voltage_chan("Dev1/ai0", min_val=0.0, max_val=10.0, terminal_config=TerminalConfiguration.DIFF)
-ch1 = nidaqmx.Task()
-ch1.ai_channels.add_ai_voltage_chan("Dev1/ai1", min_val=0.0, max_val=10.0, terminal_config=TerminalConfiguration.RSE)
+ch2 = nidaqmx.Task()
+ch2.ai_channels.add_ai_voltage_chan("Dev1/ai4", min_val=0.0, max_val=10.0, terminal_config=TerminalConfiguration.RSE)
 
 threshold = 1.5
 data = []
 count = 0
 while True:
     data0 = ch0.read()
-    data1 = ch1.read()
+    data1 = ch2.read()
     data.append([count/1000, data0, data1])
     count += 1
     val = np.array([round(data0, 4), round(data1, 4)])
@@ -32,8 +32,8 @@ while True:
 
 ch0.stop()
 ch0.close()
-ch1.stop()
-ch1.close()
+ch2.stop()
+ch2.close()
 
 data = np.array(data)
 plt.plot(data[:, 0], data[:, 1])
